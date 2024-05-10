964e9a4a4552d2d747f2b679cf06d6f27d4f905b
Markus Vickman~ <markus.vickman@hotmail.com>
2024-05-10 11:47:40 +0200

    byggt startsidan med bilder

--- src/app/start-page/start-page.component.ts
+++ src/app/start-page/start-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-start-page',
   templateUrl: './start-page.component.html',
-  styleUrl: './start-page.component.scss'
+  styleUrl: './start-page.component.scss',
+  standalone: true,
+  imports: [RouterLink]
 })
 export class StartPageComponent {
 
